183fc84e7e82fc365b78a32a10f45d9271d56cf0
Arthur <arthur2.bossu@gmail.com>
2022-01-17 16:32:43 +0100

    rotate left

--- main.c
+++ main.c
@@ -24,10 +24,10 @@ eg:
 */
 
 typedef struct tile {
-    char up, down, left, right, center; 
-    uint roads[4]; // []
-    bool meeple[9]; // sides(0-3) + corners(4-7) + middle(8)
-    bool closed_up, closed_down, closed_left, closed_right; // 
+    char up, down, left, right, center; // C = Champ, V = Ville, C = Champs, M = Monastère 
+    uint meeple;
+    bool special;
+    bool closed_up, closed_down, closed_left, closed_right;
 } tile;
 
 
@@ -37,23 +37,20 @@ void rotate_tile_clockwise(tile* t) { // untested
     t->left = t->down;
     t->down = t->right;
     t->right = tmp;
-
-    for (int i = 0; i < 4; i++)
-        if (t->roads[i] == 4) 
-            t->roads[i] = 1;
-        else
-            t->roads[i]++;
-    
-    t->meeple << 1;
-    if (t->meeple & ) {
-        t->meeple |= 0b0000001000000000;
-        t->meeple |= 0b0000000000000001;
-    }
-
 } 
+
+void rotate_tile_counter_clockwise(tile* t) { // untested
+    char tmp = t->up;
+    t->up = t->right;
+    t->right = t->down;
+    t->down = t->left;
+    t->left = tmp;
+}
 
 
 int main() {
     int joueur = 0;
+
+
 
 }
