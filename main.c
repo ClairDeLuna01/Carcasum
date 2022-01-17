1a647ae96d637eb6a3a9b7e8a0589a48fede2285
Arthur <arthur2.bossu@gmail.com>
2022-01-17 15:19:49 +0100

    commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct tile {
+    char up, down, left, right, center;
+    
+    char meeple; // 0000 0000
+    bool finisher;
+
+} tile;
+
+
+int main() {
+
+}
