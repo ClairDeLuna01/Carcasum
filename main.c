83c41f08e4561e02e6dd3643dd00d544e8fe2e44
Arthur-flush <61984952+Arthur-flush@users.noreply.github.com>
2022-01-18 22:06:56 +0100

    change linked list, unfinished

--- main.c
+++ main.c
@@ -31,109 +31,145 @@ typedef struct tile {
     bool closed[4];
 } tile;
 
+
 typedef struct list2d {
-    list2d *up, *down, *left, *right;
+    struct list2d *up, *down, *left, *right;
     tile t;
+    bool allocated;
 } list2d;
 
-list2d* List2d(tile first_tile) {
+
+
+list2d* List2d() {
     list2d* l = malloc(sizeof(list2d));
     l->up = NULL; 
     l->down = NULL; 
     l->right = NULL; 
     l->left = NULL;
-    l->t = first_tile;
     return l;
 }
 
 void link_up(list2d* list, tile new_tile) {
-    if (list->up != NULL) {
-        perror("Error, cannot link tile");
+    if (list->up == NULL) { // add a row to the top if needed
+        list2d* L = list; // we need the list to stay rectangular
+        while (L->left != NULL) { // go as far left as possible
+            L = L->left;
+        }
+        while (L != NULL) {
+            list2d* l = List2d();
+            l->allocated = false;
+            L->up = l;
+            L = L->right;
+            // add linking to other tiles 
+        }
+    }
+
+    if (list->up->allocated == true) {
+        perror("Error, cannot link tile, already linked");
         exit(1);
     }
 
-    list2d* new_link = malloc(sizeof(list2d));
-    list->up = new_link;
-    new_link->t = new_tile;
-    new_link->down = list;
+    list->up->t = new_tile;
+    list->up->down = list;
+    list->up->allocated = true;
+
 }
 
 void link_down(list2d* list, tile new_tile) {
-    if (list->up != NULL) {
-        perror("Error, cannot link tile");
+    if (list->down == NULL) { // add a row to the top if needed
+        list2d* L = list; // we need the list to stay rectangular
+        while (L->left != NULL) { // go as far left as possible
+            L = L->left;
+        }
+        while (L != NULL) {
+            list2d* l = List2d();
+            l->allocated = false;
+            L->down = l;
+            L = L->right;
+        }
+    }
+
+    if (list->down->allocated == true) {
+        perror("Error, cannot link tile, already linked");
         exit(1);
     }
 
-    list2d* new_link = malloc(sizeof(list2d));
-    list->down = new_link;
-    new_link->t = new_tile;
-    new_link->up = list;
+    list->down->t = new_tile;
+    list->down->up = list;
+    list->down->allocated = true;
 }
 
 void link_right(list2d* list, tile new_tile) {
-    if (list->up != NULL) {
-        perror("Error, cannot link tile");
+    if (list->right == NULL) { // add a row to the top if needed
+        list2d* L = list; // we need the list to stay rectangular
+        while (L->up != NULL) { // go as far up as possible
+            L = L->up;
+        }
+        while (L != NULL) {
+            list2d* l = List2d();
+            l->allocated = false;
+            L->right = l;
+            L = L->down;
+        }
+    }
+
+    if (list->right->allocated == true) {
+        perror("Error, cannot link tile, already linked");
         exit(1);
     }
 
-    list2d* new_link = malloc(sizeof(list2d));
-    list->right = new_link;
-    new_link->t = new_tile;
-    new_link->left = list;
+    list->right->t = new_tile;
+    list->right->left = list;
+    list->right->allocated = true;
 }
 
 void link_left(list2d* list, tile new_tile) {
-    if (list->up != NULL) {
-        perror("Error, cannot link tile");
+        if (list->right == NULL) { // add a row to the top if needed
+        list2d* L = list; // we need the list to stay rectangular
+        while (L->up != NULL) { // go as far up as possible
+            L = L->up;
+        }
+        while (L != NULL) {
+            list2d* l = List2d();
+            l->allocated = false;
+            L->left = l;
+            L = L->down;
+        }
+    }
+
+    if (list->left->allocated == true) {
+        perror("Error, cannot link tile, already linked");
         exit(1);
     }
 
-    list2d* new_link = malloc(sizeof(list2d));
-    list->left = new_link;
-    new_link->t = new_tile;
-    new_link->right = list;
+    list->left->t = new_tile;
+    list->left->right = list;
+    list->left->allocated = true;
 }
 
-void free_list(list2d* list) { // would be 1000% simpler in a language that supports objects
-    list2d* up = NULL;
-    list2d* down = NULL;
-    list2d* left = NULL;
-    list2d* right = NULL;
-
-
-    if (list->up != NULL) {
-        list->up->down = NULL;
-        up = list->up;
-        list->up = NULL;
+void _free_row(list2d* row) {
+    while(row->right != NULL) {
+        list2d* tmp = row->right;
+        // free tile
+        free(row);
+        row = tmp;
     }
-    if (list->down != NULL) {
-        list->down->up = NULL;
-        down = list->down;
-        list->down = NULL;
-    }
-    if (list->left != NULL) {
-        list->left->right = NULL;
-        left = list->left;
-        list->left = NULL;
-    }
-    if (list->right != NULL) {
-        list->right->left = NULL;
-        right = list->right;
-        list->right = NULL;
-    }
-    
-    if (up != NULL)
-        free_list(list->up);
-    if (down != NULL)
-        free_list(list->down);
-    if (left != NULL)
-        free_list(list->left);
-    if (right != NULL)
-        free_list(list->right);
-    
-
-    free(list);
 }
+
+void free_list(list2d* list) {
+    while(list->left != NULL)  // we go to the top left corner
+        list = list->left;
+    while(list->up != NULL)
+        list = list->up;
+    
+    while (list->down != NULL) {
+        _free_row(list);
+        list = list->down;
+    }
+    _free_row(list);
+}
+
+
 
 void rotate_tile_clockwise(tile* t) { // untested 
     char tmp = t->sides[0];
@@ -151,40 +187,21 @@ void rotate_tile_counter_clockwise(tile* t) { // untested
     t->sides[3] = tmp;
 }
 
-void alloc_tile(tile* t)
-{
-    tile *T = malloc(sizeof(tile));
-    
-    T->meeple  = 0;
-    T->special = 0;
-    
-    int i = 0;
-    
-    for(; i < 4; i++)
-    {
-        T->closed[i] = 0; 
-        T->sides[i] = "C";
-    }
-    
-    T->sides[4] = "C";
-    
-    return T;
-}
+
 
 int main() {
     // test code
     tile test_tile;
-    list2d* grid = List2d(test_tile);
+    list2d* grid = List2d();
+    grid->allocated = true; // cursed
     link_up(grid, test_tile);
     link_down(grid, test_tile);
     link_left(grid, test_tile);
     link_right(grid, test_tile);
-    link_up(grid, test_tile);
-    link_up(grid, test_tile);
-    link_up(grid, test_tile);
-    link_up(grid, test_tile);
-    link_up(grid, test_tile);
-
+    link_right(grid->down, test_tile);
+    link_left(grid->down, test_tile);
+    link_right(grid->up, test_tile);
+    link_left(grid->up, test_tile);
 
 
 }
