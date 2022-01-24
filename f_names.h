38d916b1a71f51a1df6b95fdfb1376633340af38
U-LAPTOP-QM6NMK05\arthu <arthurchateauneuf.66@gmail.com>
2022-01-24 17:54:49 +0100

    update header

--- f_names.h
+++ f_names.h
@@ -24,11 +24,11 @@ void free_grid(grid); // à faire
 
 void free_tile(tile*); // à faire
 
-int empty_grid_col(grid, int); // a faire
+int empty_grid_col(grid, int); // a faire [Arthur C]
 
-int empty_grid_row(grid, int); // a faire
+int empty_grid_row(grid, int); // a faire [Arthur C]
 
-void basic_print_grid(grid); // à faire
+void basic_print_grid(grid); // à faire [Arthur C]
 
 void add_tile_to_grid(grid, tile*); // à faire
 
