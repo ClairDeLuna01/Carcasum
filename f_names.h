36d0bd75058651ee1d7b6c22381bae954f12ef8b
Arthur <arthur2.bossu@gmail.com>
2022-01-31 14:21:31 +0100

    Update status des fonctions

--- f_names.h
+++ f_names.h
@@ -28,11 +28,11 @@ void free_grid(grid); // fait
 
 //void free_tile(tile*); // pas nécassaire 
 
-int empty_grid_col(grid, int); // a faire [Arthur C]
+int empty_grid_col(grid, int); // fait
 
-int empty_grid_row(grid, int); // a faire [Arthur C]
+int empty_grid_row(grid, int); // fait
 
-void basic_print_grid(grid); // à faire [Arthur C]
+void basic_print_grid(grid); // fait
 
 //void add_tile_to_grid(grid, tile*); // pas nécassaire 
 
