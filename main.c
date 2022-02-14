37f3742a4c0cf9bff38ec2c70f2401736920b2e2
Arthur-flush <61984952+Arthur-flush@users.noreply.github.com>
2022-02-14 19:12:05 +0100

    Blue Pill Update (actually redpilled)

--- main.c
+++ main.c
@@ -170,48 +170,47 @@ void basic_print_grid(grid G){
 tile** readcsv(char* filename) {
     tile **deck = malloc(sizeof(tile *) * 72);
     for (int i = 0; i <= N_CARDS; i++)
-    {
         deck[i] = Tile();
-    }
-    int i = 0, j = 0;
+    
+    int i = 0, j = 1;
     FILE *fp;
     char c;
+    
     fp = fopen(filename, "r");
-
-    c = fgetc(fp);
-    deck[i]->sides[j] = c;
-    j++;
+    c  = fgetc(fp);
+    deck[i]->sides[0] = c;
+    
     while (c != EOF)
     {
-        c = fgetc(fp);
-        if (c == ',')
+        
+        if (c == ',' || j == 0)
         {
             c = fgetc(fp);
-            if (c == 'a') {
+            
+            if(c == 'a') {
                 deck[i]->special = true;
                 deck[i]->sides[j] = 'p';
             }
-            else
+            
+            if(c == 'b') {
+                deck[i]->special = true;
+                deck[i]->sides[j] = 'V';
+            }
+            
+            if(c != 'a' && c != 'b')
                 deck[i]->sides[j] = c;
-            // printf("%c|",deck[i].sides[j]);
             
             j++;
         }
+        
         if (c == '\n')
         {
-            // printf("%d \n",i);
-            c = fgetc(fp);
-            // printf("%c|",c);
             i++;
             j = 0;
-            if (c == 'a') {
-                deck[i]->special = true;
-                deck[i]->sides[j] = 'p';
-            }
-            else
-                deck[i]->sides[j] = c;
-            j++;
         }
+        
+        else
+            c = fgetc(fp);
     }
 
     fclose(fp);
@@ -255,6 +254,42 @@ void printside(char side, bool special){
 		printf("\033[0;33m■\033[0m");
 }
 
+void printside(char side, bool special){
+	
+	// routes  : blanc
+	// près    : vert
+	// ville   : rouge
+	// abbaye  : cyan
+	// blason  : jaune
+	// village : violet
+	
+	if(side == 'r'){
+		printf("■");
+		return;}
+	
+	if(side == 'p' && special != 1){
+		printf("\033[0;32m■\033[0m");
+		return;}
+	
+	if(side == 'V' && special != 1){
+		printf("\033[0;31m■\033[0m");
+		return;}
+	
+	if(side == 'p' && special == 1){
+		printf("\033[0;36m■\033[0m");
+		return;}
+	
+	if(side == 'V' && special == 1){
+		printf("\033[0;33m■\033[0m");
+		return;}
+	
+	if(side == 'v'){
+		printf("\033[0;35m■\033[0m");
+		return;}
+	
+	printf("%c", side);
+}
+
 void print_grid_color(grid G){
     
     int i = 0, 
@@ -274,58 +309,143 @@ void print_grid_color(grid G){
         lastcol--;
         i--;
     }
-	
-	printf("%d %d", firstcol, lenght);
     
     for(i = 0; i < lenght; i++){
         
         if(empty_grid_row(G, i) == 0){
-            for(j = firstcol; j < lastcol; j++){
+            for(j = firstcol; j < lastcol+1; j++){
                 if(G[i][j] != NULL){
+					printf("   ");
 					printside(G[i][j]->sides[0], G[i][j]->special);
-					printside(G[i][j]->sides[0], G[i][j]->special);
-					printside(G[i][j]->sides[0], G[i][j]->special);
+					printf("  ");
 				}
-				
                 else
-                    printf("   ");
+                    printf("      ");
             }
             printf("\n");
             
-            for(j = firstcol; j < lastcol; j++){
+            for(j = firstcol; j < lastcol+1; j++){
                 if(G[i][j] != NULL){
+					printf(" ");
 					printside(G[i][j]->sides[3], G[i][j]->special);
+					printf(" ");
 					printside(G[i][j]->sides[4], G[i][j]->special);
+					printf(" ");
 					printside(G[i][j]->sides[1], G[i][j]->special);
 				}
-				
                 else
-                    printf("   ");
+                    printf("      ");
             }
             printf("\n");
             
-            for(j = firstcol; j < lastcol; j++){
+            for(j = firstcol; j < lastcol+1; j++){
                 if(G[i][j] != NULL){
+					printf("   ");
 					printside(G[i][j]->sides[2], G[i][j]->special);
-					printside(G[i][j]->sides[2], G[i][j]->special);
-					printside(G[i][j]->sides[2], G[i][j]->special);
+					printf("  ");
 				}
-            
                 else
-                    printf("   ");
+                    printf("      ");
             }
             printf("\n");
         }    
     }
 }
 
-grid create_test_grid() {
-    grid g = Grid();
-
-
-
-    return g;
+bool is_valid_pos(grid grid, tile* tile, coord coord){
+    
+    // retourne 0 si non valide, 1 si valide
+    
+    int x[4] = {coord.x,   coord.x+1, coord.x,   coord.x-1},
+        y[4] = {coord.y-1, coord.y,   coord.y+1, coord.y},
+        s1[4] = {0, 1, 2, 3},
+        s2[4] = {2, 3, 0, 1},
+        i = 0,
+        imax = 4;
+    
+    if(coord.x == 0)
+        imax = 3;
+    
+    if(coord.y == 0)
+        i = 1;
+    
+    for(; i < imax; i++)
+    {
+        //printf("%d : %d\n", s2[i], s1[i]);
+        
+        if(grid[y[i]][x[i]] != NULL){
+            
+            //printf("%c : %c\n", grid[y[i]][x[i]]->sides[s2[i]], tile->sides[s1[i]]);
+            
+            if(grid[y[i]][x[i]]->sides[s2[i]] != tile->sides[s1[i]])
+                return 0;
+        }
+    }
+    
+    return 1;
 }
+
+bool in_empty(grid grid, int i, int j){ // fonction pour gen_rand_valid_grill qui vérifie si une grilel est entourée de vide
+    if(i >= 1)
+        if(grid[i-1][j] != NULL)
+            return 0;
+    
+    if(j >= 1)
+        if(grid[i][j-1] != NULL)
+            return 0;
+        
+    if(i < N_CARDS*2)
+        if(grid[i+1][j] != NULL)
+            return 0;
+    
+    if(j < N_CARDS*2)
+        if(grid[i][j+1] != NULL)
+            return 0;
+        
+    return 1;
+}
+
+void gen_rand_valid_grill(grid grid, tile **deck){
+    coord c;
+    int try,
+        maxtry = 32000,
+        k;
+    bool used[N_CARDS] = {0};
+    srand(time(NULL));
+   
+    for(int i = 0; i < 7; i++){
+        for(int j = 0; j < 8; j++) {
+            
+            do
+                k = rand()%N_CARDS;
+            while(used[k = rand()%N_CARDS] == 1);
+            
+            c.x = j;
+            c.y = i;
+            try = 0;
+            
+            if((i == 0 && j == 0) || in_empty(grid, i, j) == 0)
+            {
+                while(try < maxtry && is_valid_pos(grid, deck[k], c) != 1)
+                {
+                    while(used[k] == 1)
+                        k = rand()%N_CARDS;
+                    
+                    rotate_tile_clockwise(deck[k]);
+                    
+                    try ++;
+                }
+                
+                if(try < maxtry)
+                {
+                    used[k] = 1;
+                    grid[i][j] = deck[k];
+                }
+            }
+        }
+    }
+}
+
 
 int main() {
     srand(time(NULL));
